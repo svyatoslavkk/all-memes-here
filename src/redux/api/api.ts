32f74a51aa8f9123b12a42b424d42b161201137a
svyatoslavkk <sv.krivonos1@gmail.com>
2024-02-06 12:01:52 +0300

    fix: fix data

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
 
 export const api = createApi({
   reducerPath: "memesApi",
