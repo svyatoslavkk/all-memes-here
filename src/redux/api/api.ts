40c11988ddf812e772c1b6b71249ad7f16766e0c
svyatoslavkk <sv.krivonos1@gmail.com>
2024-02-07 17:50:14 +0300

    fix: fix giphi api key

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+const GIPHY_API_KEY = "1zlTbIJOzZlKwgt08mHZFRC9QThRhHkL";
 
 export const api = createApi({
   reducerPath: "memesApi",
